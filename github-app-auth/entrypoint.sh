0980a3ee8bce4d1e90c0b0c99b203b28dc79d6fa
Jukka Palomäki <jukka.palomaki@paf.com>
2022-12-28 13:11:50 +0200

    Update entrypoint.sh

--- github-app-auth/entrypoint.sh
+++ github-app-auth/entrypoint.sh
@@ -8,4 +8,3 @@ access_token=""
 # TODO: https://docs.github.com/en/developers/apps/building-github-apps/authenticating-with-github-apps#authenticating-as-an-installation
 
 echo "token=$access_token" >> $GITHUB_OUTPUT
-
